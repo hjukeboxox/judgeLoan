e422b18c1d25e7f57ff9606a17008428ddaae3d6
hjukebox <hjukeboxox@gmail.com>
2024-12-30 01:00:20 +0900

    create project

---
+++ src/main/kotlin/com/zerobase/judgeLoan/JudgeLoanApplication.kt
@@ -0,0 +1,11 @@
+package com.zerobase.judgeLoan
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JudgeLoanApplication
+
+fun main(args: Array<String>) {
+	runApplication<JudgeLoanApplication>(*args)
+}
---
+++ src/test/kotlin/com/zerobase/judgeLoan/JudgeLoanApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.zerobase.judgeLoan
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JudgeLoanApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
